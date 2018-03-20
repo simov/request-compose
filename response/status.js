5d7b3f8d272dfdf679aa0796e4d43ca0d13208b7
simov <simeonvelichkov@gmail.com>
2018-03-20 18:14:29 +0200

    Update response middleware

--- response/status.js
+++ response/status.js
@@ -1,13 +1,21 @@
 
 var error = require('../utils/error')
+var redirect = require('./redirect')
 
 
-module.exports = () => ({res, body, raw = body}) => {
+module.exports = () => ({options, res, body, raw = body}) => {
 
-  if (!/^2/.test(res.statusCode)) {
-    throw error({res, body, raw})
+  if (/^2/.test(res.statusCode)) {
+    return {options, res, body, raw}
   }
-
-  return {res, body, raw}
+  else if (/^3/.test(res.statusCode)) {
+    return redirect()({options, res, body, raw})
+  }
+  else if (/^4/.test(res.statusCode)) {
+    throw error({options, res, body, raw})
+  }
+  else if (/^5/.test(res.statusCode)) {
+    throw error({options, res, body, raw})
+  }
 
 }
--- test/response/status.js
+++ test/response/status.js
@@ -3,7 +3,7 @@ var t = require('assert')
 var stream = require('stream')
 
 var Response = {
-  status: require('../../response/status')(),
+  status: require('../../response/status')({method: 'GET'}),
 }
 
 
@@ -16,27 +16,75 @@ describe('status', () => {
         body: 'hey',
       }),
       {
+        options: undefined,
         res: {statusCode: 200},
         body: 'hey',
         raw: 'hey'
       },
-      'should return the input res and body'
+      'should return the input res, body and raw arguments'
     )
   })
 
-  it('non 200 range - error message', () => {
-    t.throws(() =>
+  it('300 range', () => {
+    try {
+      Response.status({
+        options: {path: ''},
+        res: {statusCode: 301, statusMessage: 'Moved Permanently', headers: {
+          location: '/path'
+        }},
+        body: 'hey',
+        raw: 'hey',
+      })
+    }
+    catch (err) {
+      t.equal(
+        err.message,
+        'request-compose: redirect',
+        'special error message should be set while redirecting'
+      )
+      t.equal(
+        err.location,
+        '/path',
+        'location path should be set'
+      )
+    }
+  })
+
+  it('400 range', () => {
+    try {
       Response.status({
         res: {statusCode: 404, statusMessage: 'Not Found'},
         body: 'hey',
         raw: 'hey',
-      }),
-      '404 Not Found',
-      'should throw an error containing the status code and the status message'
-    )
+      })
+    }
+    catch (err) {
+      t.equal(
+        err.message,
+        '404 Not Found',
+        'should throw an error containing the status code and the status message'
+      )
+    }
   })
 
-  it('non 200 range - error properties + raw set implicitly', () => {
+  it('500 range', () => {
+    try {
+      Response.status({
+        res: {statusCode: 500, statusMessage: 'Internal Server Error'},
+        body: 'hey',
+        raw: 'hey',
+      })
+    }
+    catch (err) {
+      t.equal(
+        err.message,
+        '500 Internal Server Error',
+        'should throw an error containing the status code and the status message'
+      )
+    }
+  })
+
+  it('error properties + raw set implicitly', () => {
     try {
       Response.status({
         res: {statusCode: 404, statusMessage: 'Not Found'},
